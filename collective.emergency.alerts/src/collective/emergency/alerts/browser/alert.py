860479320e56a54537217cd3d3548775c97bf7f1
hietpasd <hietpasd@uwosh.edu>
2016-08-02 15:19:09 -0500

    added cache protocals

--- collective.emergency.alerts/src/collective/emergency/alerts/browser/alert.py
+++ collective.emergency.alerts/src/collective/emergency/alerts/browser/alert.py
@@ -159,7 +159,7 @@ class AlertsBroadcaster(BrowserView):
                         data.append(v)
                         
         # Determine Format
-        self.request.response.setHeader('ETag', md5.new(str(self._data)).hexdigest())
+        self.request.response.setHeader('ETag', md5.new(str(data)).hexdigest())
         self.request.response.setHeader('Cache-Control', 'max-age=60, s-maxage=60, public, must-revalidate')
         self.request.response.setHeader('Vary', 'Accept-Encoding')
         self.request.response.setHeader('Content-Type', 'application/json')

1c65c06967baf979a2b57332584df37bec60f18c
hietpasd <hietpasd@uwosh.edu>
2016-08-11 10:37:18 -0500

    Fixed sync bug in the broadcaster

--- collective.emergency.alerts/src/collective/emergency/alerts/browser/eas_generator.py
+++ collective.emergency.alerts/src/collective/emergency/alerts/browser/eas_generator.py
@@ -25,7 +25,7 @@ class EASGenerator(BrowserView):
         feeds = list(registry['collective.emergency.alerts.browser.controlpanel.IEmergencyAlert.global_feeds']) 
         if not feeds:
             feeds = []
-        feeds.append(str(self.portal.absolute_url() + '/eas_alerts')) # add local feed
+        feeds.append(str(self.portal.absolute_url() + '/eas_alerts').encode('ascii', 'ignore')) # add local feed
         return feeds
         
         
